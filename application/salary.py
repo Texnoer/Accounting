fe2fd7afa7010322656357d4e6da124e1ceb6c7f
Texnoer <texnoer@ro.ru>
2021-03-10 21:50:12 +0300

    Initial commit

---
+++ application/salary.py
@@ -0,0 +1,2 @@
+def calculate_salary():
+    print(f'Award for the year {250000 + 60000}')
---
+++ main.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+from application.db.people import get_employees
+from application.salary import calculate_salary
+
+if __name__ == '__main__':
+    get_employees()
+    calculate_salary()
+    date = datetime.now().date()
+    print(date)
